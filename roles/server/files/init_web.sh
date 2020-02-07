e8a1431569691515f5e5ccf9136001325bfa9400
Jon Calalang <jon.calalang@gmail.com>
2020-02-06 20:22:24 -0800

    started a new build

--- roles/server/files/init_web.sh
+++ roles/server/files/init_web.sh
@@ -1,16 +1,46 @@
 !/bin/bash
 
-##### Install Artiom's awesome Demo App #####
-# install Docker
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-# Spin up Docker Instances
+# Install Docker
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo systemctl enable docker
+sudo service docker start
+sudo usermod -aG docker cloud-user
+echo -e "Default \e[34mDocker installed\e[0m"
 
-# F5-Hello-World for HTTP Sites:
-docker run --restart=unless-stopped --name=f5-hello-world-blue -dit -p 80:8080 -p 443:8443 -e NODE='Blue' f5devcentral/f5-hello-world
-docker run --restart=unless-stopped --name=f5-hello-world-green -dit -p 82:8080 -p 444:8443 -e NODE='Green' f5devcentral/f5-hello-world
-# DVWA for f5.http iApp with WAF:
-docker run --restart=unless-stopped --name=dvwa -d -p 81:80 infoslack/dvwa
-# Hackazon for AS3 with WAF:
-docker run --restart=unless-stopped --name=hackazon -d -p 83:80 mutzel/all-in-one-hackazon:postinstall supervisord -n
-# ASM Policy Validator:
-docker run --restart=unless-stopped --name=app-sec -dit -p 445:8443 artioml/f5-app-sec
+# Create the persistant storage for our containers
+mkdir ~/Container_Content
+mkdir ~/Container_Content/consul
+sudo chown -R 1001:1001 ~/Container_Content/consul/
+mkdir ~/Container_Content/gitlab
+mkdir ~/Container_Content/gitlab/config
+mkdir ~/Container_Content/gitlab/data
+mkdir ~/Container_Content/gitlab/logs
+mkdir ~/Container_Content/nginx_html
+	
+echo -e "Default \e[97mPersistant storage for our containers created\e[0m"
+
+# Create lab containers
+sudo docker run --restart=unless-stopped -d -p 8080:80 --name nginx01 -v ~/Container_Content/nginx_html:/usr/share/nginx/html:ro nginx:latest
+sudo docker run --restart=unless-stopped -d -p 8081:3000 --name juice-shop bkimminich/juice-shop
+sudo docker run --restart=unless-stopped -d -p 8300:8300 -p 8301:8301 -p 8500:8500 -p 8600:8600 --name consul -v ~/Container_Content/consul:/bitnami bitnami/consul:latest
+sudo docker run --restart=unless-stopped -d -p 8501:80 -p 8443:443 -p 8222:22 --name gitlab --hostname gitlab.example.com -v ~/Container_Content/gitlab/config:/etc/gitlab -v ~/Container_Content/gitlab/logs:/var/log/gitlab -v ~/Container_Content/gitlab/data:/var/opt/gitlab gitlab/gitlab-ce:latest
+echo -e "Default \e[96mContainers created\e[0m"
+
+# Create static page for NGiNX
+cat > ~/Container_Content/nginx_html/index.html <<- "EOF"
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    img {
+      width: 100%;
+    }
+  </style>
+</head>
+<body>
+  <img src="https://raw.githubusercontent.com/jmcalalang/Agility-2020---AnO-Tool-Chain---Advanced-Design-Patterns-with-AS3-DO-and-TS/master/docs/intro/images/image1.png" alt="f5.com">
+</body>
+</html>"
+EOF
+echo -e "Default \e[98mNGiNX static page created\e[0m"
